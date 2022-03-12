417b8aeb639204a604390d14ff60e71f9dd79a36
Jaxon Adams <jadamsresume1@gmail.com>
2022-03-11 18:36:18 -0700

    added new prototype methods to player

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -15,3 +15,22 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
 });
+
+test('gets the player stats as an object', () => {
+    const player = new Player('Jaxon');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory for the player or returns false', () => {
+    const player = new Player('Jaxon');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
